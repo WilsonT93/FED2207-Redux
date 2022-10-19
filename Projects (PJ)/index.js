178db8fd2777b540d8236149af96430432b197c9
wilsont93 <travonnew@gmail.com>
2022-10-19 14:59:34 -0400

    Html/index/Stylesheet

---
+++ Projects (PJ)/index.js
@@ -0,0 +1,16 @@
+function tipCalc() {
+    //declare variables that handle function
+    let totalBill = document.getElementById('#billAmount').value;
+    let tipAmt = document.getElementById('#tipSelect').value;
+    let peopleAmt =  document.getElementById('#people').value;
+    
+    let afterTaxes;
+    let newBill;
+    //take in totalBill, apply taxes and return
+    afterTaxes = totalBill * .04
+    //calculate tip amount
+   let afterTip = (tipAmt * totalBill) / peopleAmt;
+    //return new bill amount
+    newBill =  + afterTaxes + afterTip 
+    console.log(newBill)
+}
